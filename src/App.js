631f6d942b67ea0ddbced38f942f139bc3527214
tanveer mir <tanveer.mir@KPDCL.local>
2024-03-29 10:03:00 +0530

    base cleaned to empty

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import AssetInduction from './components/asset-induction';
-import AssetForm from './components/remap-asset';
-import InductRemapAsset from './components/inductRemapAsset';
-import DamageEntryPage from './components/damageEntry';
-import Test from './components/TestPage';
-import InductAsset from './components/InductAsset/induct-asset';
-import ATCLoss from './components/ActLoss/actloss';
-import TransformDmg from './components/TransformerDamages/transformer-damages';
-import Mapper from './components/DataAsseteMapper/dt-asset-mapper';
 
 function App() {
   return (
@@ -16,15 +7,7 @@ function App() {
       <div className='App'>
         <header className='App-header'>
           <div>
-            {/* <AssetForm /> */}
-            {/* <AssetInduction /> */}
-            {/* <InductRemapAsset /> */}
-            {/* <DamageEntryPage /> */}
-            {/* <Test /> */}
-            {/* <InductAsset /> */}
-            {/* <ATCLoss /> */}
-            {/* <TransformDmg /> */}
-            {/* <Mapper /> */}
+           
           </div>
         </header>
       </div>
